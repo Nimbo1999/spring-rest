0c5447889145242ef9f69e9e3cd5a90a6b35ba38
Matheus Lopes <matlopes199@gmail.com>
2020-05-31 19:34:23 -0300

    Configuração do CORS

--- src/main/java/curso/api/rest/cursospringrestapi/DemoApplication.java
+++ src/main/java/curso/api/rest/cursospringrestapi/DemoApplication.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"curso.api.rest.cursospringrestapi.model"})
@@ -18,10 +20,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @RestController
 @EnableAutoConfiguration
-public class DemoApplication {
+public class DemoApplication implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+
+		registry.addMapping("/usuario/**")
+		.allowedMethods("*")
+		.allowedOrigins("http://localhost:3000");
+
+		WebMvcConfigurer.super.addCorsMappings(registry);
 	}
 
 }
--- src/main/java/curso/api/rest/cursospringrestapi/controller/IndexController.java
+++ src/main/java/curso/api/rest/cursospringrestapi/controller/IndexController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import curso.api.rest.cursospringrestapi.model.Usuario;
 import curso.api.rest.cursospringrestapi.repository.UsuarioRepository;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
